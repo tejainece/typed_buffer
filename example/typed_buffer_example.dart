b0bc4ee863b6a9242cf67aa229724b965736ed3a
Ravi Teja Gudapati <tejainece@gmail.com>
2018-07-29 20:16:00 +0200

    Architecture

---
+++ example/typed_buffer_example.dart
@@ -0,0 +1,4 @@
+import 'package:typed_buffer/typed_buffer.dart';
+
+main() {
+}
---
+++ test/typed_buffer_test.dart
@@ -0,0 +1,12 @@
+import 'package:typed_buffer/typed_buffer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    setUp(() {
+    });
+
+    test('First Test', () {
+    });
+  });
+}
